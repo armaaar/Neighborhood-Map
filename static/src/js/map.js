1706f5eac027787cfbbc8ab16826be03a1ce10c3
Ahmed Rafik <ahmedrafik_maaar@outlook.com>
2017-07-13 22:18:40 +0400

    Feat: Remove map type control

--- static/src/js/map.js
+++ static/src/js/map.js
@@ -11,6 +11,7 @@ function initMap() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
+        mapTypeControl: false
     });
     infoWindow = new google.maps.InfoWindow();
     refreshMarkers();
