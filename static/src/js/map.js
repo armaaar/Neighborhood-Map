6049b651d52820573aa7c488bd1baeaf1f69d042
Ahmed Rafik <ahmedrafik_maaar@outlook.com>
2017-07-13 21:20:38 +0400

    Feat: Load related wikipedia articles in map infowindow

--- static/src/js/map.js
+++ static/src/js/map.js
@@ -1,7 +1,11 @@
 var map;
 var markers = [];
 var infoWindow;
-
+var infoWindowTemplate = "\
+<h4>{title}</h4> \
+<hr /> \
+<h6>Related Wikipedia articles:</h6> \
+<div>{wikipedia}</div>";
 function initMap() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
@@ -48,7 +52,12 @@ function showInfoWindow(marker) {
     // Check to make sure the infoWindow is not already opened on this marker.
     if (infoWindow.marker != marker) {
         infoWindow.marker = marker;
-        infoWindow.setContent('<div>' + marker.title + '</div>');
+
+        // Show loading icon till wikipedia loads
+        setInfoWindowTemplate(marker.title, '<i class="fa fa-circle-o-notch fa-spin fa-2x fa-fw"></i>');
+        // Async Search wikipedia to get results of the place
+        setWikiInfoWindow(marker);
+        // Open infowindow
         infoWindow.open(map, marker);
         // Make sure the marker property is cleared if the infowindow is closed.
         infoWindow.addListener('closeclick', function() {
@@ -61,6 +70,50 @@ function showInfoWindow(marker) {
         }, 750);
     }
 }
+
+function setWikiInfoWindow(marker)
+{
+    // Async Search wikipedia to get results of the place
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + marker.title + '&format=json&callback=wikiCallback';
+    var wikiRequestTimeout = setTimeout(function(){
+        setInfoWindowTemplate(marker.title, "Failed to get resources. Request timed out.");
+    }, 8000);
+    //
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        jsonp: "callback",
+        success: function( response ) {
+            clearTimeout(wikiRequestTimeout);
+
+            var articleList = response[1];
+            var htmlList;
+            if(articleList.length >1)
+            {
+                htmlList = "<ul>";
+                for (var i = 0; i < articleList.length; i++) {
+                    articleStr = articleList[i];
+                    var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                    htmlList += '<li><a href="' + url + '" target="_blank">' + articleStr + '</a></li>';
+                };
+                htmlList += "</ul>";
+            } else {
+                htmlList = "There is no Wikipedia articles for this place."
+            }
+
+            setInfoWindowTemplate(marker.title, htmlList);
+        }
+    });
+}
+
+function setInfoWindowTemplate(title, wikipedia)
+{
+    var infoWindowContent = infoWindowTemplate;
+    infoWindowContent = infoWindowContent.replace('{title}', title);
+    infoWindowContent = infoWindowContent.replace('{wikipedia}', wikipedia);
+    infoWindow.setContent(infoWindowContent);
+}
+
 
 // Load gmaps
 $.getScript("http://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyBTuuVQJpSapovC0alJNf8WrW6gcrVUIbs&v=3&callback=initMap")
--- static/src/js/model.js
+++ static/src/js/model.js
@@ -1,17 +1,61 @@
 // model
-var places = [
-    {
-        title : "Place1",
-        location: {lat: 40.7713024, lng: -73.9632393},
+// Copying some places cause I'm too lazy to get my own
+var places = [{
+        title: 'Park Ave Penthouse',
+        location: {
+            lat: 40.7713024,
+            lng: -73.9632393
+        }
     },
     {
-        title : "Place2",
-        location: {lat: 40.7444883, lng: -73.9949465},
+        title: 'Chelsea Loft',
+        location: {
+            lat: 40.7444883,
+            lng: -73.9949465
+        }
     },
     {
-        title : "Place3",
-        location: {lat: 40.7347062, lng: -73.9895759},
+        title: 'Union Square Open Floor Plan',
+        location: {
+            lat: 40.7347062,
+            lng: -73.9895759
+        }
     },
+    {
+        title: 'East Village Hip Studio',
+        location: {
+            lat: 40.7281777,
+            lng: -73.984377
+        }
+    },
+    {
+        title: 'TriBeCa Artsy Bachelor Pad',
+        location: {
+            lat: 40.7195264,
+            lng: -74.0089934
+        }
+    },
+    {
+        title: 'Chinatown Homey Space',
+        location: {
+            lat: 40.7180628,
+            lng: -73.9961237
+        }
+    },
+    {
+        title: 'Trump Tower',
+        location: {
+            lat: 40.742746,
+            lng: -73.973499
+        }
+    },
+    {
+        title: 'Whitney Museum of American Art',
+        location: {
+            lat: 40.739864,
+            lng: -73.995556
+        }
+    }
 ];
 
 var shownPlaces = places;
